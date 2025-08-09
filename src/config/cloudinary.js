aee9818a9d87444fcfd0051972fb50276f607d1f
mhusnainwahid <mhusnainwahid@gmail.com>
2025-08-09 16:02:59 +0500

    done

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,22 +1,18 @@
 
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: 'dw0vns4ca',
   api_key: '974883819917529',
-  api_secret: '3OJKL3Hguk7It5XLLKq3iqRx0yw'
+  api_secret: '3OJKL3Hguk7It5XLLKq3iqRx0yw',
 });
 
-
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary: cloudinary.v2,
   params: {
     folder: 'products',
-    // format: async (req, file) => 'png', 
-    allowed_type: ['jpg', 'png']
-
+    allowed_formats: ['jpg', 'png', 'jpeg'],
   },
 });
 
