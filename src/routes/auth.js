bdfe05a11751bc61cfb65545226f3119b0d10915
mhusnainwahid <mhusnainwahid@gmail.com>
2025-08-15 19:11:48 +0500

    updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginUser, signupUser } from "../controllers/auth.js";
+import { loginUser, signupUser, verifyUser } from "../controllers/auth.js";
 
 const authRoutes = express.Router()
 
 authRoutes.post('/signup',signupUser)
 authRoutes.post('/login',loginUser)
+authRoutes.post('/verifyuser', verifyUser)
 
 export default authRoutes
